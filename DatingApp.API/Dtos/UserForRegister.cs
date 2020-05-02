46ab3c55bfa9e57c24851b2133ad0fdd788d0e29
manoj12.shrivastava@gmail.com <manoj12.shrivastava@gmail.com>
2020-05-02 13:21:33 +0530

    End of authentication

---
+++ DatingApp.API/Dtos/UserForRegister.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(6,MinimumLength =3,ErrorMessage ="You must specify password between 3 to 6 character")]
+        public string Password { get; set; }
+    }
+}
