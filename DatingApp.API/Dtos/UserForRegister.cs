617d5e0f44d7c13a4fdb2ad216264710108a9fe1
manoj12.shrivastava@gmail.com <manoj12.shrivastava@gmail.com>
2020-05-23 12:27:15 +0530

    Commit changes till 12

--- DatingApp.API/Dtos/UserForRegister.cs
+++ DatingApp.API/Dtos/UserForRegister.cs
@@ -12,7 +12,30 @@ namespace DatingApp.API.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(6,MinimumLength =3,ErrorMessage ="You must specify password between 3 to 6 character")]
+        [StringLength(8,MinimumLength =4,ErrorMessage ="You must specify password between 4 to 8 character")]
         public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string KnownAs { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
